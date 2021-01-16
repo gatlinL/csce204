8f38227dc5c3be67fe7c5ef30d021389a1255906
gatlinL <77461029+gatlinL@users.noreply.github.com>
2021-01-15 23:54:01 -0500

    Added ExercisesJan14th

---
+++ Exercises/January14th/string-intro.py
@@ -0,0 +1,15 @@
+# Learn strings and variables
+# Author: Gatlin Lawson
+
+print("I don't like quarantine")
+firstName = "Gatlin"
+lastName = "Lawson"
+print("Greetings " + firstName + " " +lastName)
+
+print(f"Welcome {firstName} {lastName}")
+
+print("I love chicken\nBut I don't like steak.")
+
+print(f"""I love Chicken
+But I don't
+like steak.""")
