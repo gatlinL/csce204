e763cd50258390a8e1c60bb6503a6e7175a39a8f
gatlinL <77461029+gatlinL@users.noreply.github.com>
2021-03-08 13:43:41 -0500

    Added Exercises March4th

---
+++ Exercises/March4th/assignments.py
@@ -0,0 +1,19 @@
+# Author: Gatlin Lawson
+
+from datetime import date, time, datetime
+
+assignments = {"Python Intro": date(2021, 1, 22),
+            "Learning Math": date(2021, 1, 30),
+            "If Statements": date(2021, 2, 5),
+            "While Loops": date(2021, 2, 14),
+            "For Loops": date(2021, 2, 27),
+            "Lists": date(2021, 3, 2),
+            "Tuples": date(2021, 3, 7)}
+
+# Loop through and display this list of assignments
+counter = 1
+
+for assignment in assignments:
+    dueDate = assignments[assignment]
+    print(f"{counter}. {assignment} - Due: " + dueDate.strftime("%m/%d/%y"))
+    counter += 1
---
+++ Exercises/March4th/birthdays1.py
@@ -0,0 +1,22 @@
+# Author: Gatlin Lawson
+
+from datetime import date, time, datetime
+
+birthdays = [date(2021, 3, 22), date(2021, 2, 20), date(2021, 5, 29),
+            date(2021, 11, 21), date(2021, 10, 22), date(2021, 3, 2),
+            date(2021, 6, 24)]
+
+closestBirthday = date(2021, 12, 31)
+
+for birthday in birthdays:
+    daysTillBirthday = (birthday - date.today()).days
+    daysTillCurrentClosest = (closestBirthday - date.today()).days
+
+    # if birthday has occured already
+    if daysTillBirthday < 0:
+        continue
+
+    if daysTillBirthday < daysTillCurrentClosest:
+        closestBirthday = birthday
+
+print("The closest birthday is " + closestBirthday.strftime("%m/%d/%y"))
