80b7d0e7d27b5f63847859b49f362b96fb61e5e4
gatlinL <77461029+gatlinL@users.noreply.github.com>
2021-03-15 18:19:59 -0400

    Added Exercises March8th

---
+++ Exercises/March8th/function-params1.py
@@ -0,0 +1,15 @@
+# Author: Gatlin Lawson
+
+def factorial(num):
+    answer = 1
+
+    if num < 0:
+        print("Invalid number")
+        return
+
+    for i in range(1, num + 1):
+        answer *= i
+    
+    print(f"{num}! = {answer}")
+
+factorial(7)
---
+++ Exercises/March8th/functions1.py
@@ -0,0 +1,10 @@
+# Author: Gatlin Lawson
+
+def say_hello():
+    print("Hello World")
+
+def say_goodbye():
+    print("Goodbye")
+
+say_hello()
+say_goodbye()
